ba5a18d679831a07621900f7369ede95b6cac83f
Dmytro Kovalchuk <rocky61100@gmail.com>
2021-02-10 15:07:01 +0200

    three

--- 1_11.js
+++ 1_11.js
@@ -17,3 +17,21 @@
 
 // 3
 
+let numb = prompt("numb : ")
+function fun(numb){
+    for(let i = 1; i <= numb; i++) {
+        if(i % 3 ==0 && i % 5 ==0 ){
+            console.log("buzzfizz");
+        }
+        else if (i % 3 == 0) {
+            console.log("fizz");
+        }
+         else if (i % 5 == 0) {
+            console.log("buzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fun(numb);
