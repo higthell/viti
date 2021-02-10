87d56aaae341b741151bb23beacaa5db496392c4
Dmytro Kovalchuk <rocky61100@gmail.com>
2021-02-10 14:36:45 +0200

    two

---
+++ 1_11.js
@@ -0,0 +1,19 @@
+// 1
+
+// let total=prompt("Numb : ");
+// function seconds(total) {
+//     return total/60;
+// }
+// console.log(seconds(total));
+
+// 2
+
+// let side = prompt("length : ")
+// let count = prompt("count : ")
+// function perimeter(side , count){
+//     return side*count;
+// }
+// console.log(perimeter(side, count));
+
+// 3
+
